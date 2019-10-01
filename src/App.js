4f5d9341c185ff2d4e210dcf60c23b25e84fb129
Migara <evamarcod@gmail.com>
2019-10-01 17:02:24 +0200

    Add renfe info and Iberia info

--- src/App.js
+++ src/App.js
@@ -4,27 +4,23 @@ import List from './components/List';
 import Detail from './components/Detail';
 import JSZip from 'jszip';
 
-
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+
+    };
     this.getInputFile= this.getInputFile.bind(this);
     this.handleFile = this.handleFile.bind(this);
-    // this.handleBackgroundImage = this.handleBackgroundImage.bind(this);
-    // this.handleForegroundImage = this.handleForegroundImage.bind(this);
-    // this.handleJsonData = this.handleJsonData.bind(this);
   }
+
   handleForegroundImage = (zip, filename) => {
-    console.log(filename);
     zip
     .file(filename)
     .async('base64')
     .then(function success(content) {
-      console.log(content);
-      // $element.prepend('<img src="data:image/png;base64,' + content + '">');
+      
     }, function error(e) {
-        // handle the error
         console.log('error', e);
     });
   };
@@ -35,70 +31,112 @@ class App extends React.Component {
     .file(filename)
     .async('base64')
     .then(function success(content) {
-      console.log(content);
-      // $element.css('background-image', 'url(data:image/png;base64,' + content + ')');
-    }, function error(e) {
-        // handle the error
+        
+      }, function error(e) {
         console.log('error', e);
-    });
+      }
+    );
   };
+
   handleJsonData =  (zip, filename) => {
-    console.log(filename)
     zip
     .file(filename)
     .async('string')
     .then(function success(content) {
-      const passData = JSON.parse(content);     
-      console.log(passData);
+      const passData = JSON.parse(content);   
+      if( passData.organizationName === "Iberia"){
+        const primColor = passData.backgroundColor;
+        const origin = passData.boardingPass.primaryFields[0].value;
+        const originName = passData.boardingPass.primaryFields[0].label;
+        const destination = passData.boardingPass.primaryFields[1].value;
+        const destinationName = passData.boardingPass.primaryFields[1].label;
+        const departureDate = passData.boardingPass.headerFields[0].value;
+        const departureTime = passData.boardingPass.backFields[4].value;
+        // const boardingTime = passData.boardingTime.auxiliaryFields[2].value;
+        const arrivalTime = passData.boardingPass.backFields[6].value;
+
+        // const terminal = '';
+        const flight = passData.boardingPass.backFields[7].value;
+        const flyingClass = passData.boardingPass.backFields[12].value;
+        const seat = passData.boardingPass.secondaryFields[1].value;
+        const passengerName = passData.boardingPass.backFields[0].value;
+
+        console.log(primColor);
+        console.log(origin);
+        console.log(originName);
+        console.log(destination);
+        console.log(destinationName);
+        console.log(departureDate);
+        console.log(departureTime);
+        // console.log(boardingTime);
+        console.log(arrivalTime);
+        console.log(flight);
+        console.log(flyingClass);
+        console.log(seat);
+        console.log(passengerName);
+      } 
+      else if(passData.organizationName === "Renfe"){
+        const primColor = passData.labelColor;
+        const originName = passData.boardingPass.primaryFields[0].label;
+        const destinationName = passData.boardingPass.primaryFields[1].label;
+        const departureDate = passData.boardingPass.headerFields[0].value;
+        const departureTime = passData.boardingPass.primaryFields[0].value;
+        const arrivalTime = passData.boardingPass.primaryFields[1].value;
+
+        const train = passData.boardingPass.auxiliaryFields[0].value;
+        const car = passData.boardingPass.auxiliaryFields[1].value;
+        const seat = passData.boardingPass.auxiliaryFields[2].value;
+        const trainClass = passData.boardingPass.auxiliaryFields[3].value;
+
+        console.log(primColor);
+        console.log(originName);
+        console.log(destinationName);
+        console.log(departureDate);
+        console.log(departureTime);
+        console.log(arrivalTime);
+        console.log(train);
+        console.log(trainClass);
+        console.log(seat);
+        console.log(car);
+      }
+      else{
+
+      }
+      
     })
-  }
+  };
 
   handleFile(file) {
-    console.log(file.name)
-    console.log(file)
     const handleJsonData = this.handleJsonData;
     const handleForegroundImage = this.handleForegroundImage;
     const handleBackgroundImage = this.handleBackgroundImage;
     JSZip.loadAsync(file)                                   
       .then(function(zip) {
         zip.forEach(function (index, zipEntry) {
-          console.log(zipEntry);
-          console.log(zipEntry.name)
-        switch (zipEntry.name) {
-          case 'pass.json':
-            handleJsonData(zip, zipEntry.name);
-            break;
-          case 'logo.png':
-            handleForegroundImage(zip, zipEntry.name);
-            break;
-          case 'logo@2x.png':
-            handleForegroundImage(zip, zipEntry.name);
-            break;
-          case 'icon.png':
-            handleForegroundImage(zip, zipEntry.name);
-            break;
-          case 'icon@x2.png.png':
-            handleForegroundImage(zip, zipEntry.name);
-            break;
-          case 'thumbnail.png':
-            handleForegroundImage(zip, zipEntry.name);
-            break;
-          case 'background.png':
-            handleBackgroundImage(zip, zipEntry.name);
-            break;
-          case 'strip.png':
-            handleBackgroundImage(zip, zipEntry.name);
-            break;
-          case 'manifest.json':
-            handleJsonData(zip, zipEntry.name);
-            break;
-          default:
-            console.log('default');
-            break;
+          switch (zipEntry.name) {
+            case 'pass.json':
+              handleJsonData(zip, zipEntry.name);
+              break;
+            case 'logo.png':
+              handleForegroundImage(zip, zipEntry.name);
+              break;
+            case 'logo@2x.png':
+              handleForegroundImage(zip, zipEntry.name);
+              break;
+            case 'icon.png':
+              handleForegroundImage(zip, zipEntry.name);
+              break;
+            case 'icon@x2.png.png':
+              handleForegroundImage(zip, zipEntry.name);
+              break;
+            default:
+              console.log('default');
+              break;
+            }
           }
-
-        })}
-      )
+        )
+      }
+    )
 }
   getInputFile(event){
     console.log(event.target.files);
