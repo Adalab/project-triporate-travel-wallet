6d1d8c01e20c6d8768c6b06630d19fb80922cacf
Constanza Sosa <constanza.sosa.g@gmail.com>
2019-10-01 16:31:37 +0200

    Add button to trigger input file

--- src/components/List.js
+++ src/components/List.js
@@ -4,6 +4,18 @@ import './List.scss';
 import QRCode from 'qrcode.react';
 
 class List extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.myFileField = React.createRef();
+
+    this.handleFilePicker = this.handleFilePicker.bind(this);
+  }
+
+  handleFilePicker() {
+    this.myFileField.current.click();
+  }
+
   render() {
     const {getInputFile} = this.props;
     const Array = [1, 2, 3];
@@ -12,14 +24,28 @@ class List extends React.Component {
         <ul>
         {Array.map((item, index) =>{return(<li key= {index}> <Link to={`/detail/${item}`} >Aqui irá la tarjeta {item}</Link></li>)})}
         </ul>
-       <input type="file" name="" id="" className="input" onChange={getInputFile}/>
-       <QRCode value="755190101463160911600002706190518300510B115885YS3..CFTUB" 
-       bgColor="#FF0080"
-       fgColor="#FFFFFF"
-       size="200"
-       includeMargin="true"
-       //{passData.barcode.message}
-        />
+        <button
+         className="button"
+         type="button"
+         name="add_img"
+         onClick={this.handleFilePicker}
+       >+
+       </button>
+       <input
+         type="file"
+         ref={this.myFileField}
+         name="image"
+         id="img-selector"
+         className=""
+         onChange={getInputFile}
+       />
+        <QRCode value="755190101463160911600002706190518300510B115885YS3..CFTUB" 
+        bgColor="#FF0080"
+        fgColor="#FFFFFF"
+        size="200"
+        includeMargin="true"
+        //{passData.barcode.message}
+          />
       </div>
     );
   }
