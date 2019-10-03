c88d664d0f63e55dfc06338d5583fbdca8ffb1a8
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-10-03 08:55:14 +0200

    Print qr big detail

--- src/components/QrDetail.js
+++ src/components/QrDetail.js
@@ -8,19 +8,20 @@ class QrDetail extends React.Component {
     const { routerProps } = this.props;
     console.log('hola soy el qr grande')
       return(
-        <div className="qr__detail-wrapper2">
-          <Link className="link-go-back" to = {`/detail/${routerProps.match.params.id}`}>
-            <span className="go-back-icon">&lt;</span>
-          </Link>  
+        <div className="qr__detail-color-wrapper">
+          <div className="qr__link-go-bak-wrapper ">
+            <Link className="qr__link-go-back" to = {`/detail/${routerProps.match.params.id}`}>
+              <span className="go-back-icon">&lt;</span>
+            </Link>
+          </div> 
           <div className="qr__small-wrapper">
-          <h1>soy el qr detail JEJE</h1>
             <QRCode
               value="755190101463160911600002706190518300510B115885YS3..CFTUB" 
               bgColor="#FFFF"
               fgColor="#000"
               size={200}
               includeMargin={true}
-              className="qr__small"
+              className="detail-qr__small"
               //{passData.barcode.message}
             /> 
           </div>
