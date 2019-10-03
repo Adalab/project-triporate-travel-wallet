449ab80f07d8b41e81b23f736fdf783e95b4bd5a
Constanza Sosa <constanza.sosa.g@gmail.com>
2019-10-03 15:15:04 +0200

    Add Docs

--- README.md
+++ README.md
@@ -36,7 +36,7 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 You have to run `npm start` to use the app in the development mode. 
 
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser. The page will reload if you make edits.
+Open [https://localhost:3000](https://localhost:3000) to view it in the browser. The page will reload if you make edits.
 
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
---
+++ docs/precache-manifest.1554e7884cd46f2707542affec11d013.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "307dd1534ff46fc339bad42c5693aeee",
+    "url": "./index.html"
+  },
+  {
+    "revision": "179a0f7088c1da14864b",
+    "url": "./static/css/main.da2d0fa2.chunk.css"
+  },
+  {
+    "revision": "f8537443757ef5fa0ec1",
+    "url": "./static/js/2.063b1859.chunk.js"
+  },
+  {
+    "revision": "179a0f7088c1da14864b",
+    "url": "./static/js/main.5855908c.chunk.js"
+  },
+  {
+    "revision": "20eca475f6736fe9a0ce",
+    "url": "./static/js/runtime-main.d2017d1d.js"
+  }
+]);
