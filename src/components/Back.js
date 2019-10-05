40dcdf7dd68b8274c8b535b46828e0bb08554574
Constanza Sosa <constanza.sosa.g@gmail.com>
2019-10-05 11:17:38 +0200

    Add mediaquery

--- src/components/Back.js
+++ src/components/Back.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import './Back.scss';
 import PropTypes from 'prop-types';
+import List from './List';
+
 
 class Back extends React.Component {
 
@@ -13,54 +15,66 @@ class Back extends React.Component {
 
     if (boardingPass) {
       if (boardingPass.organizationName === 'Iberia') {
-        return(
-          <div 
-            className="back__wrapper"
-            style={{ backgroundColor: `#D7192D` }}
-            >
-            <Link className="back__link-go-back" to = {`/detail/${routerProps.match.params.id}`}>
-            <i className="fas fa-angle-left"></i>
-            </Link>  
-    
-            <div className="back__info-wrapper">     
-            <div className="back__card">
-                  <div className="back__card__header">
-                    <img src={`data:image/png;base64,${boardingPass.logo}`} 
-                    alt="Organization logo" 
-                    className="back_logo"/>
-                </div>
-                  <div className="back__user-name">
-                    <p className="user__flyer-title">FREQUENT FLYER</p>
-                    <p className="user__flyer-content">{boardingPass.frequentFlyer}</p>
-                  </div>
-                  <div className="back__user-name">
-                    <p className="user__ticket-title">TICKET NUMBER</p>
-                    <p className="user__ticket-content">{boardingPass.ticketNumber}</p>
-                  </div>
-                  <div className="back__user-name">
-                    <p className="user__operator-title">OPERATOR</p>
-                    <p className="user__operator-content">{boardingPass.operator}</p>
-                  </div>
-                  <div className="back__user-name">
-                    <p className="user__code-title">BOOKING CODE</p>
-                    <p className="user__code-content">{boardingPass.bookingCode}</p>
-                  </div>
-                  <div className="back__user-name">
-                    <p className="user__terminal-title">TERMINAL</p>
-                    <p className="user__terminal-content">{boardingPass.terminal}</p>
-                  </div>
-                  <div className="back__user-name">
-                      <p className="user__gatehour-title">OTHERS</p>
-                      <p className="user__gatehour-content"></p>
-                  </div>
-                  </div>
+        return (
+          <React.Fragment>
+            <div className="detail__list">
+                  <List
+                    boardingList={boardingList}
+                  />
             </div>
-          </div>
-      )
+            <div 
+              className="back__wrapper"
+              style={{ backgroundColor: `#D7192D` }}
+              >
+                <Link className="back__link-go-back" to = {`/detail/${routerProps.match.params.id}`}>
+                <i className="fas fa-angle-left"></i>
+                </Link>  
+                <div className="back__info-wrapper">     
+                  <div className="back__card">
+                      <div className="back__card__header">
+                        <img src={`data:image/png;base64,${boardingPass.logo}`} 
+                        alt="Organization logo" 
+                        className="back_logo"/>
+                      </div>
+                      <div className="back__user-name">
+                        <p className="user__flyer-title">FREQUENT FLYER</p>
+                        <p className="user__flyer-content">{boardingPass.frequentFlyer}</p>
+                      </div>
+                      <div className="back__user-name">
+                        <p className="user__ticket-title">TICKET NUMBER</p>
+                        <p className="user__ticket-content">{boardingPass.ticketNumber}</p>
+                      </div>
+                      <div className="back__user-name">
+                        <p className="user__operator-title">OPERATOR</p>
+                        <p className="user__operator-content">{boardingPass.operator}</p>
+                      </div>
+                      <div className="back__user-name">
+                        <p className="user__code-title">BOOKING CODE</p>
+                        <p className="user__code-content">{boardingPass.bookingCode}</p>
+                      </div>
+                      <div className="back__user-name">
+                        <p className="user__terminal-title">TERMINAL</p>
+                        <p className="user__terminal-content">{boardingPass.terminal}</p>
+                      </div>
+                      <div className="back__user-name">
+                          <p className="user__gatehour-title">OTHERS</p>
+                          <p className="user__gatehour-content"></p>
+                      </div>
+                    </div>
+                </div>
+            </div>
+          </React.Fragment>
+          )
       }
 
       else if(boardingPass.organizationName === 'Renfe') {
         return(
+          <React.Fragment>
+            <div className="detail__list">
+                  <List
+                    boardingList={boardingList}
+                  />
+            </div>
           <div className="back__wrapper"
               style={{ backgroundColor: `${boardingPass.backColor}` }}
             >
@@ -102,6 +116,7 @@ class Back extends React.Component {
               </div>
             </div>
           </div>
+          </React.Fragment>
         )
       }
 
